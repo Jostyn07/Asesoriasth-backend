ea99fbddff80f89197ec4cf954c5ce82fe651d6f
Jostyn07 <jostynag075@gmail.com>
2025-08-14 15:12:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,22 +17,10 @@ const DRIVE_FOLDER_ID = process.env.GOOGLE_DRIVE_FOLDER_ID;
 
 let auth;
 try {
-    const secretFilePath = process.env.NODE_ENV === 'production' 
-        ? '/etc/secrets/Documentos_json' 
-        : path.join(__dirname, 'Documentos.json');
-
-    if (fs.existsSync(secretFilePath)) {
-        const credentialsContent = fs.readFileSync(secretFilePath, 'utf8');
-        const credentials = JSON.parse(credentialsContent);
-        
-        auth = new google.auth.GoogleAuth({
-            credentials: credentials,
-            scopes: ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
-        });
-        console.log('Autenticación de Google configurada.');
-    } else {
-        throw new Error('No se encontró el archivo de credenciales.');
-    }
+    auth = new google.auth.GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+    });
+    console.log('Autenticación de Google configurada.');
 } catch (error) {
     console.error('Error al configurar la autenticación de Google:', error);
     process.exit(1);
